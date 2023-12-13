220f7521684b44adf8215abf339a3a1f021c2910
hojintop <hojintop@naver.com>
2023-12-13 18:12:27 +0900

    BuildContext context first main2.dart

---
+++ lib/main2.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(main2());
+}
+
+class main2 extends StatefulWidget {
+  @override
+  State<main2> createState() => _main2State();
+}
+
+class _main2State extends State<main2> {
+  List<int> numbers = [];
+
+  void addNumber() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+            fontSize: 60,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              W_Numbers(),
+              for (var count in numbers)
+                Text(
+                  '$count',
+                ),
+              IconButton(
+                onPressed: addNumber,
+                icon: Icon(
+                  Icons.add_box_rounded,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class W_Numbers extends StatelessWidget {
+  const W_Numbers({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Numbers',
+      style: Theme.of(context).textTheme.titleLarge,
+    );
+  }
+}
